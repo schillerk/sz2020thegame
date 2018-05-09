f00c6bdab5410110abac49abe5d8b2bda53fd6c9
schillerk <kyle@Kyles-MacBook-Pro-4.local>
2018-05-08 18:30:02 -0700

    simpler example

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
+# import time
+# import RPi.GPIO as GPIO
+
+# lp = 4
+# bp = 17
+
+# print "hey"
+
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(lp, GPIO.OUT)
+# # GPIO.setup(bp, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# while True:
+# 	GPIO.output(bp, True)
+
 import time
 import RPi.GPIO as GPIO
 
-lp = 4
-bp = 17
-
-print "hey"
-
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(lp, GPIO.OUT)
-GPIO.setup(bp, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(4, GPIO.OUT)
 while True:
-	GPIO.output(bp, True)
+	GPIO.output(4, True)
+	time.sleep(.5)
+	GPIO.output(4, False)
