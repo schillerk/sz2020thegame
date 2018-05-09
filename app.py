c4173d1b5784c3a2ebf13d0adc707c80db4212f4
schillerk <kyle@Kyles-MacBook-Pro-4.local>
2018-05-08 18:27:41 -0700

    testing led

---
+++ app.py
@@ -0,0 +1,14 @@
+import time
+import RPi.GPIO as GPIO
+
+lp = 4
+bp - 17
+
+print "hey"
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(lp, GPIO.OUT)
+GPIO.setup(bp, GPIO.IO, pull_up_down=GPIO.PUD_DOWN)
+while True:
+	GPIO.output(bp, True)
